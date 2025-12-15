3e13ea2ee76cf05a4d90f782e8fa4036dc1b63b2
mohammed-Eltayeb <ahmeedsofarees@gmail.com>
2025-12-16 00:07:21 +0200

    Update api-key.php

--- api-key.php
+++ api-key.php
@@ -3,7 +3,8 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); // للسماح بالوصول من النطاق الأمامي
 
 // المفتاح الذي زودتني به
-$apiKey = 'AIzaSyC6_zOQuR67ikUPvKMR7WkSf1wgd8dZ0C8';
+$apiKey = 'AIzaSyBfJW8I08SvdNRFqyvIQnHEPWYNbyYOrOA';
 
 echo json_encode(['key' => $apiKey]);
 ?>
+
